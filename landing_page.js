ef1af56a4c3bf23679a924993bdc24a4adc30160
shaanzie <ishaanlagwankar@gmail.com>
2019-07-29 14:38:19 +0530

    Added voting page and minor changes on landing page

---
+++ landing_page.js
@@ -0,0 +1,20 @@
+function validate()
+{
+    var uid = document.getElementById('inp').value;
+    window.localStorage.setItem('User',uid);
+    req = {usr:uid}
+    $.ajax({
+        type : 'POST',
+        url : 'dummyurl.com',
+        data : JSON.stringify(req),
+        contentType : "application/json",
+        success : function(data)    {
+            console.log(req);
+            window.location.replace('voting.html')
+        },
+        error : function()  {
+            window.location.reload();
+            alert("Error loading credentials. Please try again.");
+        } 
+    });
+}
